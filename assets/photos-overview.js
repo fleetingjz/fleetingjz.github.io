86e4076345cabafad10670f18f8a2e87260cdd3e
Phy <git@phy25.com>
2014-07-24 12:20:11 +0800

    初步完成 机位总览。

--- assets/photos-overview.js
+++ assets/photos-overview.js
@@ -1,6 +1,16 @@
 $(function(){
 	$('#overviewer').html('<p>正在加载... 时间过长，则加载失败。</p>');
 	$.getJSON('assets/photos-overview.json', function(data, s){
-		console.log(data);
+		if(!data.base || !data.photos.length > 0){
+			$('#overviewer').html('<p>加载失败：文件错误。</p>');
+			return false;
+		}
+		$('#overviewer').html('<ul id="o-v1"></ul>');
+		for(i in data.photos){
+			var a = data.photos[i], b = a.split('-');
+			b[0] = b[0].replace('fjz');
+			var date = b[1].substr(0, 4)+'-'+b[1].substr(4, 2)+'-'+b[1].substr(6, 2);
+			$('#o-v1').append('<li><a href="'+data.base+a+'!inline'+'" target="_blank"><img src="'+data.base+a+'!thumb'+'" alt="机位 '+b[0]+'" /></a><br /><p>机位 '+b[0]+'（<time datetime="'+date+'">'+b[1]+'</time>） <a href="'+data.base+a+'!large'+'" target="_blank">特大图</a></p></li>');
+		}
 	});
 });
