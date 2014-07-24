6f8e9b0c6cbdaa515d9814c653a70affed05fe67
Phy <git@phy25.com>
2014-07-24 12:24:54 +0800

    修复日期显示和界面显示。

--- assets/photos-overview.js
+++ assets/photos-overview.js
@@ -8,9 +8,10 @@ $(function(){
 		$('#overviewer').html('<ul id="o-v1"></ul>');
 		for(i in data.photos){
 			var a = data.photos[i], b = a.split('-');
-			b[0] = b[0].replace('fjz');
+			b[0] = b[0].replace('fjz', '');
+			b[2] = b[2].substr(0, 2)+':'+b[2].substr(2, 2);;
 			var date = b[1].substr(0, 4)+'-'+b[1].substr(4, 2)+'-'+b[1].substr(6, 2);
-			$('#o-v1').append('<li><a href="'+data.base+a+'!inline'+'" target="_blank"><img src="'+data.base+a+'!thumb'+'" alt="机位 '+b[0]+'" /></a><br /><p>机位 '+b[0]+'（<time datetime="'+date+'">'+b[1]+'</time>） <a href="'+data.base+a+'!large'+'" target="_blank">特大图</a></p></li>');
+			$('#o-v1').append('<li><a href="'+data.base+a+'!inline'+'" target="_blank"><img src="'+data.base+a+'!thumb'+'" alt="机位 '+b[0]+'" /></a><br /><p>机位 '+b[0]+'（<time datetime="'+date+'" title="北京时间 '+date+' '+b[2]+'">'+b[1]+'</time>） <a href="'+data.base+a+'!large'+'" target="_blank">特大图</a></p></li>');
 		}
 	});
 });
